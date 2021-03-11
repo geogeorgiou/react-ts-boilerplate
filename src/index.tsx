a2ea970a808263089cd7260d32a845bdb042f805
geogeorgiou <georgiogeo27@gmail.com>
2021-03-11 10:58:08 +0200

    Setting up the core structure with App.tsx and index.tsx files

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => {
-    // (window as any).hello();
-    return <div>Hello World</div>
-}
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "./components/App";
 
 ReactDOM.render(<App/>, document.getElementById("root"));
