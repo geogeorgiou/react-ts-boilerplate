0706e30a782448f6c18eb48c051012247621c1a4
geogeorgiou <georgiogeo27@gmail.com>
2021-03-16 12:49:54 +0200

    Setting up the core list render

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Recorder from "../Recorder/Recorder";
+import Calendar from "../Calendar/Calendar";
 
 const App: React.FC = () => {
 
   return (
       <div className="App">
         <Recorder/>
+        <Calendar/>
       </div>
   );
 };
